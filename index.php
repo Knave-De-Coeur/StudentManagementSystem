31f8c41667c17479e145da1544a301a5ced23812
Alexander Mifsud <alexanderm.1496@gmail.com>
2018-09-07 18:23:19 +0200

    Init Commit - Added gitignore first file and started structure.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Management System</title>
+</head>
+<body>
+    <?php echo "Welcome to the Student Management System"; ?>
+</body>
+</html>
